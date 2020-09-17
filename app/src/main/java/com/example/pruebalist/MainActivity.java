9d067e31a769257503759de0c34c0215bee6dd5a
Gerardo Zelaya <57957674+GERARD2619-17@users.noreply.github.com>
2020-09-16 21:30:55 -0600

    prueba 5

--- app/src/main/java/com/example/pruebalist/MainActivity.java
+++ app/src/main/java/com/example/pruebalist/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//ya hice otra prueba 24554345
+//ya hice otra prueba que ondas te sale
         tv1 = (TextView)findViewById(R.id.tv1);
         lv1 = (ListView)findViewById(R.id.lv1);
 
